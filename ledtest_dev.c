b524b7b0676a0ccef483e564dd255d7d0e88951b
HongJiHo <wlghlplp@ajou.ac.kr>
2018-06-08 06:09:28 +0000

    ledtest modify

--- ledtest_dev.c
+++ ledtest_dev.c
@@ -11,17 +11,20 @@
 
 #define GPIO1 18
 #define DEV_NAME "ledtest_dev"
-#define DEV_NUM 240
+#define DEV_NUM 250
 #define COUNT_READ _IOR(100, 0, int)
 
 MODULE_LICENSE("GPL");
 
-static int count = 0;
+static int count;
+static int lock = 0;
 struct task_struct *task;
 struct cdev my_cdev;
 dev_t devno;
 int major;
 static char *msg = NULL;
+
+int sound_thread(void *data);
 
 module_param(major, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
 MODULE_PARM_DESC(major, "major");
@@ -33,75 +36,66 @@ int ledtest_open(struct inode *pinode, struct file *pfile){
 
 ssize_t ledtest_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset){
 	printk("Write simple char drv\n");
-
+	
 	if(copy_from_user(msg, buffer, length)){
 		printk("Write error\n");
 		return -1;
 	}
+	printk("copyfrom success\n");
 	return length;
 }
 
 ssize_t ledtest_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset){
 	printk("Read ledtest drv\n");
 
-	if(strcmp(msg, "test") == 0){
-		copy_to_user(buffer, "GET TEST", length);
-	}
-	else{
-		copy_to_user(buffer, "NOT TEST", length);
-	}
+	kthread_stop(task);
+
+	printk("count: %d\n", count);
+
+	char StrCount[2];
+	StrCount[0] = '0' + count;
+	StrCount[1] = '\0';
+	
+	copy_to_user(buffer, StrCount, length);
+	
+	printk("copyto success : msg : %s, buffer: %s\n", msg, buffer);
+	printk("copyto success : msg : %s, buffer: %s\n", msg, buffer);
+	
+	task = kthread_run(sound_thread, NULL, "sound thread");
+
 	return 0;
 }
 
-
-long ledtest_ioctl(struct file *pfile, unsigned int command, unsigned long arg)
-{
-	printk(KERN_ALERT "Read ledtest drv\n");
-	
-	char* counnnt;
-	counnnt[0] = '0' + count;
-	counnnt[1] = '\0';	
-	
-	printk("counnnt : %s\n", counnnt);
-
-	switch(command){
-		case COUNT_READ:
-			printk("copy_to_user start\n");
-			copy_to_user((void*)arg, (void*)counnnt, (ssize_t)2);
-			printk("copy_to_user done\n");
-			break;
-		default:
-			printk("Not found command..\n");
-			break;
-	}
-	
-	return 0;
-}
 int ledtest_close(struct inode *pinode, struct file *pfile){
 	printk(KERN_ALERT, "RELEASE ledtest_dev\n");
 	return 0;
 }
 
 int sound_thread(void *data){
-	
+	count = 0;
+	int seq = 0;
 	while(1){
 		int value = gpio_get_value(3);
-		printk("gpio value = %d\n", value);
+		//printk("gpio value = %d\n", value);
 		
-		int x = 0;
 		if(value == 0){
 			count++;
 		}
+		else{
+			
+		}
+
+		int x = 0;
 		while(x < 500000){
 			x++;
 		}
 		//delay(10);
 		usleep_range(1000,1000);
 		if(kthread_should_stop())
-
 			break;
 
 	}
+	return 0;
 
 }
 
@@ -110,15 +104,14 @@ struct file_operations fop = {
 	.open = ledtest_open,
 	.read = ledtest_read,
 	.write = ledtest_write,
-	.unlocked_ioctl = ledtest_ioctl,
 	.release = ledtest_close,
 };
 
 int __init ledtest_init(void){
 	printk(KERN_ALERT "INIT ledtest_dev\n");
 	
-	alloc_chrdev_region(&devno, 0, 1, "ledtest_dev");
-	major = MAJOR(devno);
+	devno = MKDEV(major, 0);
+	register_chrdev_region(devno, 1, "ledtest_dev");
 	cdev_init(&my_cdev, &fop);
 	my_cdev.owner = THIS_MODULE;
 
@@ -135,6 +128,7 @@ int __init ledtest_init(void){
 	if(gpio_direction_input(3) < 0){
 		printk("gpio_direction_input fail\n");
 	}
+	
 	task = kthread_run(sound_thread, NULL, "sound thread");
 
 	return 0;
@@ -147,6 +141,7 @@ void __exit ledtest_exit(void){
 	kthread_stop(task);
 	gpio_free(3);
 	unregister_chrdev(DEV_NUM, DEV_NAME);
+	cdev_del(&my_cdev);
 }
 
 module_init(ledtest_init);
